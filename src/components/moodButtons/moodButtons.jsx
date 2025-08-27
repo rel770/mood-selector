0111486ee00e94c57d97028e0ce4473cc390143c
rel700 <rel@mail.com>
2025-08-27 17:36:44 +0300

    Replace Button component with MoodButton for improved mood selection

--- src/components/moodButtons/moodButtons.jsx
+++ src/components/moodButtons/moodButtons.jsx
@@ -1,10 +1,10 @@
-import Button from "../button/button";
+import MoodButton from "../moodButton/moodButton";
 import "./moodButtons.css";
 
 export default ({ moods, onMoodSelect }) => (
   <div className="mood-buttons">
     {moods.map((m) => (
-      <Button key={m.mood} {...m} onClick={onMoodSelect} />
+      <MoodButton key={m.mood} mood={m.mood} emoji={m.emoji} onClick={onMoodSelect} />
     ))}
   </div>
 );
